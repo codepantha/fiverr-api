52318c30247b1e30a0765ac0ff28e50a668c6899
codepanther <pezechigozirim@gmail.com>
2023-11-02 10:42:26 +0100

    Setup stripe payments

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -1,8 +1,9 @@
+import Stripe from 'stripe';
+
 import Gig from '../models/gig.model.js';
 import Order from '../models/order.model.js';
 
 export const index = async (req, res, next) => {
-  console.log({ 'USER ID': req.userId, 'IS SELLER': req.isSeller})
   try {
     const orders = await Order.find({
       ...(req.isSeller ? { sellerId: req.isSeller } : { buyerId: req.userId }),
@@ -15,8 +16,20 @@ export const index = async (req, res, next) => {
   }
 };
 
-export const create = async (req, res, next) => {
-  const gig = await Gig.findById(req.body.gigId);
+export const intent = async (req, res, next) => {
+  const stripe = new Stripe(process.env.STRIPE);
+
+  console.log('REQ ID', req.params.id);
+
+  const gig = await Gig.findById(req.params.id);
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: gig.price * 100,
+    currency: 'usd',
+    automatic_payment_methods: {
+      enabled: true
+    }
+  });
 
   const newOrder = new Order({
     gigId: gig._id,
@@ -25,9 +38,29 @@ export const create = async (req, res, next) => {
     buyerId: req.userId,
     sellerId: gig.userId,
     price: gig.price,
-    payment_intent: 'temporary'
-  })
+    payment_intent: paymentIntent.id
+  });
 
   await newOrder.save();
-  res.status(201).json(newOrder)
-}
+  res.status(201).json({
+    clientSecret: paymentIntent.client_secret,
+    order: newOrder
+  });
+};
+
+export const confirmOrder = async (req, res, next) => {
+  try {
+    const order = await Order.findOneAndUpdate(
+      { payment_intent: req.body.payment_intent },
+      {
+        $set: {
+          isCompleted: true
+        }
+      }
+    );
+
+    res.status(200).send('Order has been confirmed.');
+  } catch (err) {
+    next(err);
+  }
+};
--- routes/order.routes.js
+++ routes/order.routes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { verifyToken } from '../middleware/jwt.js';
-import { create, index } from '../controllers/order.controller.js';
+import { index, intent, confirmOrder } from '../controllers/order.controller.js';
 
 const router = express.Router();
 
-router.route('/').get(verifyToken, index).post(verifyToken, create);
+router.route('/').get(verifyToken, index).put(verifyToken, confirmOrder)
+router.post('/create-payment-intent/:id', verifyToken, intent)
 
 export default router;
