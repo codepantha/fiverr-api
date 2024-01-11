9e94ab9c5792570722d62c0dd48882d24f2fdc3b
codepanther <pezechigozirim@gmail.com>
2024-01-11 16:01:42 +0100

    Update cors for deployment

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ mongoose.set('strictQuery', true);
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: 'http://127.0.0.1:5173', credentials: true }));
+app.use(cors({ origin: 'https://gighouse.netlify.app', credentials: true }));
 
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/users', userRoutes);
